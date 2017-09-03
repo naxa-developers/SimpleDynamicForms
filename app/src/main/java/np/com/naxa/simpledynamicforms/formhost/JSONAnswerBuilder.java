0ee0c93df88a250d7c678f78d0e3eb1d75d23217
Nishon Tandukar <nishon.tan@gmail.com>
2017-09-03 12:26:09 +0545

    Refactored JSONAnswerBuilder

--- app/src/main/java/np/com/naxa/simpledynamicforms/formhost/JSONAnswerBuilder.java
+++ app/src/main/java/np/com/naxa/simpledynamicforms/formhost/JSONAnswerBuilder.java
@@ -1,19 +1,18 @@
 package np.com.naxa.simpledynamicforms.formhost;
 
 
-import android.util.Log;
+import android.text.TextUtils;
 
 import com.google.gson.Gson;
-import com.orhanobut.logger.Logger;
 
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.Predicate;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Collection;
 import java.util.List;
 
+import np.com.naxa.simpledynamicforms.Dump;
 import np.com.naxa.simpledynamicforms.savedform.Form;
 import np.com.naxa.simpledynamicforms.savedform.QuestionAnswer;
 import timber.log.Timber;
@@ -35,44 +34,64 @@ public class JSONAnswerBuilder {
         filledForm = new Form();
     }
 
-    @SuppressWarnings("unchecked")
-    public void addAnswer(final QuestionAnswer newAnswer) {
+
+    public void addAnswer(QuestionAnswer newAnswer) {
+
 
 //        #REF https://www.leveluplunch.com/java/examples/find-element-in-list/
-
-
         Timber.i(" %s save answer in index %s", newAnswer.getAnswer(), newAnswer.getOrder());
 
+
+        List<QuestionAnswer> commitedQuestionAnswer = getAnswer(newAnswer);
+
+        if (commitedQuestionAnswer.isEmpty()) {
+            //does not have any question for that order
+            //so just add the object to list
+
+            filledForm.addQuestionAnswer(newAnswer, filledForm.getQuestionAnswers());
+        } else {
+            QuestionAnswer oldAnswer = commitedQuestionAnswer.get(0);
+            filledForm.setQuestionAnswer(oldAnswer.getOrder(), newAnswer, filledForm.getQuestionAnswers());
+
+        }
+    }
+
+    private QuestionAnswer removeNullAnswer(QuestionAnswer newAnswer) {
+
+        boolean isAnswerisNull = TextUtils.isEmpty(newAnswer.getAnswer());
+
+        Timber.i(" is answer null %s", isAnswerisNull);
+
+        if (isAnswerisNull) {
+            newAnswer.setAnswer("No Answer Provided");
+
+            Dump.object("JSONAnswerBuilder", newAnswer);
+        }
+
+
+        return newAnswer;
+    }
+
+    /**
+     * Checks if the a answer already exist for a question in the list
+     *
+     * @return 0 if none exist, should only return 0 or 1;
+     */
+    @SuppressWarnings("unchecked")
+    private List<QuestionAnswer> getAnswer(final QuestionAnswer newAnswer) {
         Predicate condition = new Predicate() {
             public boolean evaluate(Object sample) {
                 return ((QuestionAnswer) sample).getQuestion().equals(newAnswer.getQuestion());
             }
         };
 
-        List<QuestionAnswer> commitedQuestionAnswer = (List<QuestionAnswer>) CollectionUtils.select(filledForm.getQuestionAnswers(), condition);
-
-
-        if (commitedQuestionAnswer.isEmpty()) {
-
-
-            //does not have any question for that order
-            //so just add the object to list
-
-            filledForm.addQuestionAnswer(newAnswer, filledForm.getQuestionAnswers());
-        } else {
-
-            QuestionAnswer oldAnswer = commitedQuestionAnswer.get(0);
-            filledForm.setQuestionAnswer(oldAnswer.getOrder(), newAnswer, filledForm.getQuestionAnswers());
-
-        }
-
+        return (List<QuestionAnswer>) CollectionUtils.select(filledForm.getQuestionAnswers(), condition);
 
     }
 
     public String getFinalizedForm(String formName) {
         Gson gson = new Gson();
         filledForm.setFormName(formName);
-
         return gson.toJson(filledForm);
 
     }
